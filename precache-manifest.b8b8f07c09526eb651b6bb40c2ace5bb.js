b3d08df09510471453513fd9cc913473f29970d5
Nicolas S <sir.ago@gmail.com>
2020-07-22 22:21:47 +0200

    Updates

---
+++ precache-manifest.b8b8f07c09526eb651b6bb40c2ace5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c83ceffd3ca4e94d620d158181cbde6",
+    "url": "/react-weather-demo/index.html"
+  },
+  {
+    "revision": "added29786ac29c1c037",
+    "url": "/react-weather-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db6df23a78b2497bf495",
+    "url": "/react-weather-demo/static/js/2.458e0997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-demo/static/js/2.458e0997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "added29786ac29c1c037",
+    "url": "/react-weather-demo/static/js/main.69390014.chunk.js"
+  },
+  {
+    "revision": "8ea11a1a3d95f3fef0fe",
+    "url": "/react-weather-demo/static/js/runtime-main.99bd36a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
